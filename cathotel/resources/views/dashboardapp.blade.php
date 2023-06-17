5127cef5fbf440bbd9381d7ff68b2b54c82a90b4
Okta P <57696739+oktaaaa@users.noreply.github.com>
2023-06-18 06:54:56 +0700

    dashboard

--- cathotel/resources/views/dashboardapp.blade.php
+++ cathotel/resources/views/dashboardapp.blade.php
@@ -19,19 +19,9 @@
                   <div class="avatar flex-shrink-0">
                     <img src="../assets/img/icons/unicons/chart-success.png" alt="chart success" class="rounded">
                   </div>
-                  <div class="dropdown">
-                    <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="bx bx-dots-vertical-rounded"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
                 </div>
-                <span class="fw-semibold d-block mb-1">Profit</span>
-                <h3 class="card-title mb-2">$12,628</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
+                <span>Hewan</span>
+                <h3 class="card-title mb-2">{{count($hewans)}}</h3>
               </div>
             </div>
           </div>
@@ -42,19 +32,9 @@
                   <div class="avatar flex-shrink-0">
                     <img src="../assets/img/icons/unicons/wallet-info.png" alt="Credit Card" class="rounded">
                   </div>
-                  <div class="dropdown">
-                    <button class="btn p-0" type="button" id="cardOpt6" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="bx bx-dots-vertical-rounded"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
                 </div>
-                <span>Sales</span>
-                <h3 class="card-title text-nowrap mb-1">$4,679</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+                <span>Ruangan</span>
+                <h3 class="card-title text-nowrap mb-1">{{count($rooms)}}</h3>
               </div>
             </div>
           </div>
@@ -65,19 +45,11 @@
                   <div class="avatar flex-shrink-0">
                     <img src="../assets/img/icons/unicons/wallet-info.png" alt="Credit Card" class="rounded">
                   </div>
-                  <div class="dropdown">
-                    <button class="btn p-0" type="button" id="cardOpt6" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="bx bx-dots-vertical-rounded"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
+                  
                 </div>
-                <span>Sales</span>
-                <h3 class="card-title text-nowrap mb-1">$4,679</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+                <span>Staff</span>
+                <h3 class="card-title text-nowrap mb-1">Belum</h3>
+                
               </div>
             </div>
           </div>
@@ -89,25 +61,140 @@
                   <div class="avatar flex-shrink-0">
                     <img src="../assets/img/icons/unicons/wallet-info.png" alt="Credit Card" class="rounded">
                   </div>
-                  <div class="dropdown">
-                    <button class="btn p-0" type="button" id="cardOpt6" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="bx bx-dots-vertical-rounded"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
+                  
                 </div>
-                <span>Sales</span>
-                <h3 class="card-title text-nowrap mb-1">$4,679</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+                <span>Customer</span>
+                <h3 class="card-title text-nowrap mb-1">{{count($customers)}}</h3>
+                
               </div>
             </div>
           </div>
         </div>
-        
+
+        <div class="col-lg-12 order-2 order-md-3 order-lg-2 mb-4">
+          <div class="card">
+            <div class="row row-bordered g-0"> 
+              <h5 class="card-header m-0 me-2 pb-3">Total Revenue</h5>
+            </div>
+            {{-- grafik --}}
+          <script src="https://code.highcharts.com/highcharts.js"></script>
+          <script src="https://code.highcharts.com/modules/exporting.js"></script>
+          <script src="https://code.highcharts.com/modules/export-data.js"></script>
+          <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
+          <figure class="highcharts-figure">
+              <div id="container"></div>
+              <p class="highcharts-description">
+                  A basic column chart comparing emissions by pollutant.
+                  Oil and gas extraction has the overall highest amount of
+                  emissions, followed by manufacturing industries and mining.
+                  The chart is making use of the axis crosshair feature, to highlight
+                  years as they are hovered over.
+              </p>
+          </figure>
+
+          {{-- css --}}
+          <style>
+              .highcharts-figure,
+              .highcharts-data-table table {
+                  min-width: 310px;
+                  max-width: 800px;
+                  margin: 1em auto;
+              }
+
+              #container {
+                  height: 400px;
+              }
+
+              .highcharts-data-table table {
+                  font-family: Verdana, sans-serif;
+                  border-collapse: collapse;
+                  border: 1px solid #ebebeb;
+                  margin: 10px auto;
+                  text-align: center;
+                  width: 100%;
+                  max-width: 500px;
+              }
+
+              .highcharts-data-table caption {
+                  padding: 1em 0;
+                  font-size: 1.2em;
+                  color: #555;
+              }
+
+              .highcharts-data-table th {
+                  font-weight: 600;
+                  padding: 0.5em;
+              }
+
+              .highcharts-data-table td,
+              .highcharts-data-table th,
+              .highcharts-data-table caption {
+                  padding: 0.5em;
+              }
+
+              .highcharts-data-table thead tr,
+              .highcharts-data-table tr:nth-child(even) {
+                  background: #f8f8f8;
+              }
+
+              .highcharts-data-table tr:hover {
+                  background: #f1f7ff;
+              }
+
+          </style>
+
+          {{-- js --}}
+
+          <script>
+              Highcharts.chart('container', {
+              chart: {
+                  type: 'column'
+              },
+              title: {
+                  text: 'Monthly Average Rainfall'
+              },
+              subtitle: {
+                  text: 'Source: WorldClimate.com'
+              },
+              xAxis: {
+                  categories: [
+                      'a', 'b' , 'c'
+                  ],
+                  crosshair: true
+              },
+              yAxis: {
+                  min: 0,
+                  title: {
+                      text: 'Rainfall (mm)'
+                  }
+              },
+              tooltip: {
+                  headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                  pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                      '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                  footerFormat: '</table>',
+                  shared: true,
+                  useHTML: true
+              },
+              plotOptions: {
+                  column: {
+                      pointPadding: 0.2,
+                      borderWidth: 0
+                  }
+              },
+              series: [{
+                  name: 'Mahasiswa',
+                  data: [1,2,3]
+
+              }]
+          });
+          </script>
+          </div>
+          
+        </div>   
       </div>
+      
     </div>
 
 @endsection
